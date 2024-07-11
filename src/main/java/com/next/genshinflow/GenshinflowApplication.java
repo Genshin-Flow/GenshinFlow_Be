5948292522c639e9bda538efd563ddd884074c4c
demuu <preasim.kr@gmail.com>
2024-07-11 21:37:51 +0900

    Initial project setup and configuration

---
+++ src/main/java/com/next/genshinflow/GenshinflowApplication.java
@@ -0,0 +1,13 @@
+package com.next.genshinflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinflowApplication.class, args);
+	}
+
+}
