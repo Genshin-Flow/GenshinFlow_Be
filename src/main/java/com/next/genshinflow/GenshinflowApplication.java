060e83fb9bb8ae7ce0727ad2c809c210c5f6a142
demuu <preasim.kr@gmail.com>
2024-07-18 22:17:16 +0900

    기초 세팅, exception 추가

--- src/main/java/com/next/genshinflow/GenshinflowApplication.java
+++ src/main/java/com/next/genshinflow/GenshinflowApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenshinflowApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GenshinflowApplication.class, args);
 	}
-	// 푸쉬 테스트
 }
