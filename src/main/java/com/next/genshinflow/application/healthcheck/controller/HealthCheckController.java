06295a27fa0832a06f7d5adcc9a0d17cb0f0b25b
BaeRoNuI <xogh20321@gmail.com>
2024-08-02 01:13:02 +0900

    feat: demo api 삭제 및 HealthCheckController 추가

---
+++ src/main/java/com/next/genshinflow/application/healthcheck/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.next.genshinflow.application.healthcheck.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/healthcheck")
+@RestController
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("UP");
+    }
+}
--- src/main/java/com/next/genshinflow/controller/response/DemoResponse.java
+++
@@ -1,7 +0,0 @@
-package com.next.genshinflow.controller.response;
-
-public record DemoResponse(
-    boolean isUp
-) {
-
-}
