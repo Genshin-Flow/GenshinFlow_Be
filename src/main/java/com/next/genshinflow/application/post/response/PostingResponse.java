05565ab90de2cfb9e73ef30d71407ea3049e60b3
BaeRoNuI <xogh20321@gmail.com>
2024-08-08 22:15:18 +0900

    feat: FE 논의에 따른 api spec 변경

--- src/main/java/com/next/genshinflow/application/post/response/PostingResponse.java
+++ src/main/java/com/next/genshinflow/application/post/response/PostingResponse.java
@@ -1,18 +1,15 @@
 package com.next.genshinflow.application.post.response;
 
+import com.next.genshinflow.enumeration.QuestCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
-public record PostResponse(
+public record PostingResponse(
     @Schema(description = "포스트 id", type = "long", example = "1")
     long id,
-    // TODO: member/user 작업시 수정
-    // @Schema(description = "포스트 id", type = "long", example = "1")
     String assigner,
-    // TODO: member/entity 작업 분 merge 시 수정
-    // @Schema(description = "퀘스트 종류", type = "QuestCategory", example = "something")
-    // QuestCategory category
-    String category,
+    @Schema(description = "퀘스트 종류", type = "QuestCategory", example = "something")
+    QuestCategory category,
     @Schema(description = "월드 레벨", type = "int", example = "7")
     int wordLevel,
     @Schema(description = "이야기 (요청글 제목)", type = "String", example = "집가고싶어요")
--- src/main/java/com/next/genshinflow/application/post/response/PostsResponse.java
+++
@@ -1,9 +0,0 @@
-package com.next.genshinflow.application.post.response;
-
-import java.util.List;
-
-public record PostsResponse(
-    List<PostResponse> posts
-) {
-
-}
