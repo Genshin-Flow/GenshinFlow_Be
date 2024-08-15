fe0ce6d22b1429025e9077d2d180a8ffa5c05ecc
BaeRoNuI <xogh20321@gmail.com>
2024-08-16 00:04:20 +0900

    feat: 끌올을 위해 Setter 추가

--- src/main/java/com/next/genshinflow/domain/BaseEntity.java
+++ src/main/java/com/next/genshinflow/domain/BaseEntity.java
@@ -6,10 +6,14 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
