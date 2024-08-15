54ee539bb6211eb2c99643dcd9e97e2c3798d8b0
BaeRoNuI <xogh20321@gmail.com>
2024-08-16 02:41:27 +0900

    feat: MemberResponse 에 uid 추가 및 id 에 jsonIgnore 적용

--- src/main/java/com/next/genshinflow/application/user/response/MemberResponse.java
+++ src/main/java/com/next/genshinflow/application/user/response/MemberResponse.java
@@ -1,12 +1,16 @@
 package com.next.genshinflow.application.user.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.next.genshinflow.enumeration.AccountStatus;
 import com.next.genshinflow.enumeration.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MemberResponse(
+    @JsonIgnore
     @Schema(description = "유저 id", type = "long", example = "1")
     long id,
+    @Schema(description = "유저 uid", type = "long", example = "1")
+    long uid,
     @Schema(description = "닉네임", type = "String", example = "nickname")
     String name,
     @Schema(description = "이메일", type = "String", example = "example@gmail.com")
