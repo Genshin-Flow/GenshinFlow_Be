e49977e00a235df9309749f6c1d1ab97e7f4beb5
BaeRoNuI <xogh20321@gmail.com>
2024-08-16 01:20:43 +0900

    feat: Posting API Path 추가

--- src/main/java/com/next/genshinflow/application/posting/controller/publicapi/PostingAPI.java
+++ src/main/java/com/next/genshinflow/application/posting/controller/publicapi/PostingAPI.java
@@ -2,9 +2,15 @@ package com.next.genshinflow.application.posting.controller.publicapi;
 
 
 import com.next.genshinflow.application.PageResponse;
+import com.next.genshinflow.application.posting.request.PostingCreateRequest;
+import com.next.genshinflow.application.posting.request.PostingDeleteRequest;
+import com.next.genshinflow.application.posting.request.PostingModifyRequest;
+import com.next.genshinflow.application.posting.request.PostingPullUpRequest;
 import com.next.genshinflow.application.posting.response.PostingResponse;
+import com.next.genshinflow.application.user.response.MemberResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.awt.print.Pageable;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Posting API", description = "포스팅 API")
@@ -15,46 +21,50 @@ public interface PostingAPI {
         description = "최근 포스팅을 size 개 가져옵니다. 최대개수 : 100"
     )
     ResponseEntity<PageResponse<PostingResponse>> getRequests(
-        long size
+        Pageable pageable
     );
 
     @Operation(summary = "비회원 리퀘스트 작성", description = "비회원의 리퀘스트를 등록합니다.")
     ResponseEntity<PostingResponse> createRequestByNonMember(
-
+        PostingCreateRequest postingCreateRequest
     );
 
     @Operation(summary = "회원 리퀘스트 작성", description = "회원의 리퀘스트를 등록합니다")
     ResponseEntity<PostingResponse> createRequestByUser(
-
+        MemberResponse memberResponse,
+        PostingCreateRequest postingCreateRequest
     );
 
     @Operation(summary = "비회원 리퀘스트 수정", description = "비회원의 리퀘스트를 수정합니다.")
     ResponseEntity<PostingResponse> modifyRequestByNonMember(
-
+        PostingModifyRequest postingModifyRequest
     );
 
     @Operation(summary = "회원 리퀘스트 수정", description = "회원의 리퀘스트를 수정합니다.")
     ResponseEntity<PostingResponse> modifyRequestByUser(
-
+        MemberResponse memberResponse,
+        PostingModifyRequest postingModifyRequest
     );
 
     @Operation(summary = "비회원 리퀘스트 삭제", description = "비회원의 리퀘스트를 삭제합니다.")
     ResponseEntity<PostingResponse> deleteRequestByNonMember(
-
+        PostingDeleteRequest postingDeleteRequest
     );
 
     @Operation(summary = "회원 리퀘스트 삭제", description = "회원의 리퀘스트를 삭제합니다.")
     ResponseEntity<PostingResponse> deleteRequestByUser(
-
+        MemberResponse memberResponse,
+        PostingDeleteRequest postingDeleteRequest
     );
 
     @Operation(summary = "비회원 리퀘스트 끌어올리기", description = "비회원의 리퀘스트을 끌올합니다.")
     ResponseEntity<PostingResponse> pullUpRequestByNonMember(
-
+        PostingPullUpRequest postingPullUpRequest
     );
 
     @Operation(summary = "회원 리퀘스트 끌어올리기", description = "회원의 리퀘스트를 끌올합니다.")
     ResponseEntity<PostingResponse> pullUpRequestByUser(
-
+        MemberResponse memberResponse,
+        PostingPullUpRequest postingPullUpRequest
     );
 }
--- src/main/java/com/next/genshinflow/application/posting/controller/publicapi/PostingController.java
+++ src/main/java/com/next/genshinflow/application/posting/controller/publicapi/PostingController.java
@@ -1,11 +1,24 @@
 package com.next.genshinflow.application.posting.controller.publicapi;
 
 import com.next.genshinflow.application.PageResponse;
+import com.next.genshinflow.application.posting.request.PostingCreateRequest;
+import com.next.genshinflow.application.posting.request.PostingDeleteRequest;
+import com.next.genshinflow.application.posting.request.PostingModifyRequest;
+import com.next.genshinflow.application.posting.request.PostingPullUpRequest;
 import com.next.genshinflow.application.posting.response.PostingResponse;
+import com.next.genshinflow.application.user.response.MemberResponse;
 import com.next.genshinflow.auth.UserAuth;
+import java.awt.print.Pageable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -13,44 +26,78 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostingController implements PostingAPI {
 
+    private static final String LOGIN_HEADER = "";
+    private static final String NO_LOGIN_HEADER = "!" + LOGIN_HEADER;
 
-    public ResponseEntity<PageResponse<PostingResponse>> getRequests(long size) {
+
+    @GetMapping
+    public ResponseEntity<PageResponse<PostingResponse>> getRequests(
+        @RequestParam Pageable pageable
+    ) {
         return null;
     }
 
-    public ResponseEntity<PostingResponse> createRequestByNonMember() {
-        return null;
-    }
-
-    @UserAuth
-    public ResponseEntity<PostingResponse> createRequestByUser() {
-        return null;
-    }
-
-    public ResponseEntity<PostingResponse> modifyRequestByNonMember() {
-        return null;
-    }
-
-    @UserAuth
-    public ResponseEntity<PostingResponse> modifyRequestByUser() {
-        return null;
-    }
-
-    public ResponseEntity<PostingResponse> deleteRequestByNonMember() {
+    @PostMapping(value = "/create", headers = LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> createRequestByNonMember(
+        @RequestBody PostingCreateRequest postingCreateRequest
+    ) {
         return null;
     }
 
     @UserAuth
-    public ResponseEntity<PostingResponse> deleteRequestByUser() {
+    @PostMapping(value = "/create", headers = NO_LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> createRequestByUser(
+        @AuthenticationPrincipal MemberResponse memberResponse,
+        @RequestBody PostingCreateRequest postingCreateRequest
+    ) {
         return null;
     }
 
-    public ResponseEntity<PostingResponse> pullUpRequestByNonMember() {
+    @PutMapping(value = "/modify", headers = LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> modifyRequestByNonMember(
+        @RequestBody PostingModifyRequest postingModifyRequest
+    ) {
         return null;
     }
 
     @UserAuth
-    public ResponseEntity<PostingResponse> pullUpRequestByUser() {
+    @PutMapping(value = "/modify", headers = NO_LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> modifyRequestByUser(
+        @AuthenticationPrincipal MemberResponse memberResponse,
+        @RequestBody PostingModifyRequest postingModifyRequest
+    ) {
+        return null;
+    }
+
+    @DeleteMapping(value = "/delete", headers = LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> deleteRequestByNonMember(
+        @RequestBody PostingDeleteRequest postingDeleteRequest
+    ) {
+        return null;
+    }
+
+    @UserAuth
+    @DeleteMapping(value = "/delete", headers = NO_LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> deleteRequestByUser(
+        @AuthenticationPrincipal MemberResponse memberResponse,
+        @RequestBody PostingDeleteRequest postingDeleteRequest
+    ) {
+        return null;
+    }
+
+    @PutMapping(value = "/pullup", headers = LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> pullUpRequestByNonMember(
+        @RequestBody PostingPullUpRequest postingPullUpRequest
+    ) {
+        return null;
+    }
+
+    @UserAuth
+    @PutMapping(value = "/pullup", headers = NO_LOGIN_HEADER)
+    public ResponseEntity<PostingResponse> pullUpRequestByUser(
+        @AuthenticationPrincipal MemberResponse memberResponse,
+        @RequestBody PostingPullUpRequest postingPullUpRequest
+    ) {
         return null;
     }
 }
