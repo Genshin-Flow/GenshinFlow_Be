776c11173b65eb7f951c307128c2a3aaba253a66
demuu <preasim.kr@gmail.com>
2024-09-12 21:02:45 +0900

    feat: login, token dto 추가

---
+++ src/main/java/com/next/genshinflow/application/user/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.next.genshinflow.application.user.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+}
