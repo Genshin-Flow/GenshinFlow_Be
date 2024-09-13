762a4b59f35b784d3d53f664d51b38a05bebfd25
demuu <preasim.kr@gmail.com>
2024-09-13 16:41:29 +0900

    feat: AuthorityEntity 추가

--- src/main/java/com/next/genshinflow/domain/user/entity/MemberEntity.java
+++ src/main/java/com/next/genshinflow/domain/user/entity/MemberEntity.java
@@ -5,15 +5,11 @@ import com.next.genshinflow.domain.BaseEntity;
 import com.next.genshinflow.enumeration.AccountStatus;
 import com.next.genshinflow.enumeration.Role;
 import com.next.genshinflow.enumeration.converter.AccountStatusConverter;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -49,4 +45,12 @@ public class MemberEntity extends BaseEntity {
 
     @Column(name = "role")
     private Role role;
+
+    @ManyToMany
+    @JoinTable(
+        name = "member_authority",
+        joinColumns = {@JoinColumn(name = "member_id", referencedColumnName = "member_id")},
+        inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
+    )
+    private Set<AuthorityEntity> authorities;
 }
