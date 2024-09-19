1943c887c9bb455484d76ea718606618ade7afac
demuu <preasim.kr@gmail.com>
2024-09-19 09:39:27 +0900

    feat: AuthorityResponse 추가

---
+++ src/main/java/com/next/genshinflow/application/user/dto/AuthorityResponse.java
@@ -0,0 +1,12 @@
+package com.next.genshinflow.application.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityResponse {
+    private String authorityName;
+}
