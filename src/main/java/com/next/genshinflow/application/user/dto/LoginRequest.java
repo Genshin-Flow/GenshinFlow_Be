930008e4317aa8894bafeac6750c1e69faa3887c
demuu <preasim.kr@gmail.com>
2024-09-23 09:22:34 +0900

    refactor: 사용되지 않는 import 구문 제거

--- src/main/java/com/next/genshinflow/application/user/dto/LoginRequest.java
+++ src/main/java/com/next/genshinflow/application/user/dto/LoginRequest.java
@@ -2,13 +2,11 @@ package com.next.genshinflow.application.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
