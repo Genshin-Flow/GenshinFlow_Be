b4de09d68acf1993c3866ca1d913da0c4d179871
demuu <preasim.kr@gmail.com>
2024-09-23 09:21:16 +0900

    fix: 테스트 코드 수정

--- src/test/java/com/next/genshinflow/intergration/security/SecurityTests.java
+++ src/test/java/com/next/genshinflow/intergration/security/SecurityTests.java
@@ -1,128 +1,69 @@
 package com.next.genshinflow.intergration.security;
 
+import com.next.genshinflow.application.user.dto.LoginRequest;
+import com.next.genshinflow.application.user.dto.SignUpRequest;
+import com.nimbusds.jose.shaded.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class SecurityTests {
-
     @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("회원가입 완료 후 리다이렉션")
+    private static final long UID = 12345678L;
+    private static final String EMAIL = "test@gmail.com";
+    private static final String PASSWORD = "testPW1234!";
+
+    @DisplayName("회원가입 요청 테스트")
     @Test
-    void signUpWithValidInput() throws Exception {
-        String email = "valid@gmail.com";
-        String password = "testPW!";
+    void signUpTest() throws Exception {
+        SignUpRequest signUpRequest = SignUpRequest.builder()
+            .uid(UID)
+            .email(EMAIL)
+            .password(PASSWORD)
+            .build();
 
-        ResultActions actions = mockMvc.perform(post("/sign-up")
-                .param("email", email)
-                .param("password", password));
+        String json = new Gson().toJson(signUpRequest);
 
-        actions.andExpect(status().is3xxRedirection())
-            .andExpect(flash().attributeExists("successMessage"))
-            .andExpect(redirectedUrl("/login"));
+        ResultActions resultActions = mockMvc.perform(post("/auth/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json));
+
+        resultActions.andExpect(status().isCreated());
     }
 
-    @DisplayName("입력값에 오류가 있다면 회원가입 실패하고 회원가입 페이지에 오류 메시지 표시")
+    @DisplayName("로그인 요청 테스트")
     @Test
-    void signUpWithWrongInput() throws Exception {
-        String invalidEmail = "invalid-email";
-        String shortPassword = "1234";
+    void signInTest() throws Exception {
+        signUpTest();
 
-        ResultActions actions = mockMvc.perform(post("/sign-up")
-                .param("email", invalidEmail)
-                .param("password", shortPassword));
+        LoginRequest loginRequest = LoginRequest.builder()
+            .email(EMAIL)
+            .password(PASSWORD)
+            .build();
 
-        actions.andExpect(status().isOk())
-            .andExpect(model().attributeHasFieldErrors("email", "password"))
-            .andExpect(view().name("sign-up"));
-    }
+        String json = new Gson().toJson(loginRequest);
 
-    @DisplayName("로그인 성공 후 리다이렉션")
-    @Test
-    void loginWithValidCredentials() throws Exception {
-        String email = "valid@gmail.com";
-        String password = "testPW!";
+        ResultActions resultActions = mockMvc.perform((post("/auth/signin")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json)));
 
-        mockMvc.perform(post("/sign-up")
-            .param("email", email)
-            .param("password", password));
-
-        ResultActions actions = mockMvc.perform(post("/login")
-                .param("email", email)
-                .param("password", password));
-
-        actions.andExpect(status().is3xxRedirection())
-            .andExpect(redirectedUrl("/"));
-    }
-
-    @DisplayName("로그인 실패 시 실패 메시지 노출")
-    @Test
-    void loginWithInvalidCredentials() throws Exception {
-        String invalidEmail = "invalidUser";
-        String invalidPassword = "invalidPassword";
-
-        ResultActions actions = mockMvc.perform(post("/login")
-                .param("email", invalidEmail)
-                .param("password", invalidPassword));
-
-        actions.andExpect(status().is3xxRedirection())
-            .andExpect(redirectedUrl("/login?error"))
-            .andExpect(flash().attributeExists("errorMessage"));
-    }
-
-    @DisplayName("인증 없이 공개된 엔드포인트에 접근하는 테스트")
-    @Test
-    void accessUnsecuredEndpoint() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/"));
-
-        actions.andExpect(status().isOk());
-    }
-
-    @DisplayName("인증 없이 보호된 엔드포인트에 접근")
-    @Test
-    void accessSecuredEndpointWithoutAuthentication() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/admin"));
-
-        actions.andExpect(status().isUnauthorized());
-    }
-
-    @DisplayName("USER 역할을 가진 사용자가 마이페이지 엔드포인트에 접근")
-    @Test
-    @WithMockUser(username = "user", roles = {"USER"})
-    void accessSecuredEndpointWithUserRole() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/member/my-page"));
-
-        actions.andExpect(status().isOk());
-    }
-
-    @DisplayName("ADMIN 역할을 가진 사용자가 관리자 엔드포인트에 접근")
-    @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
-    void accessAdminEndpointWithAdminRole() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/admin"));
-
-        actions.andExpect(status().isOk());
-    }
-
-    @DisplayName("USER 역할을 가진 사용자가 관리자 엔드포인트에 접근")
-    @Test
-    @WithMockUser(username = "user", roles = {"USER"})
-    void accessAdminEndpointWithUserRole() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/admin"));
-
-        actions.andExpect(status().isForbidden());
+        resultActions.andExpect(status().isOk());
     }
 }
