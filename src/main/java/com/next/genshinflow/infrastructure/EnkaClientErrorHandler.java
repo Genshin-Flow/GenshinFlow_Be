57bcf62f85d8a5c2f6f205461fab6d909c064aee
demuu <preasim.kr@gmail.com>
2024-10-15 17:27:51 +0900

    feat: EnkaClientErrorHandler 추가

---
+++ src/main/java/com/next/genshinflow/infrastructure/EnkaClientErrorHandler.java
@@ -0,0 +1,18 @@
+package com.next.genshinflow.infrastructure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class EnkaClientErrorHandler extends DefaultResponseErrorHandler {
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        log.error("Error occurred while calling external API: HTTP Status Code {}, Message: {}",
+            response.getStatusCode(), response.getStatusText());
+
+        super.handleError(response);
+    }
+}
