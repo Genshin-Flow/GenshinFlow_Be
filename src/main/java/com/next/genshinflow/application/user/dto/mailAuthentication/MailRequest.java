02e141a98e738d94e08c8c42618d63f26ff352d0
demuu <preasim.kr@gmail.com>
2024-10-18 18:51:25 +0900

    feat: MailRequest 추가

---
+++ src/main/java/com/next/genshinflow/application/user/dto/mailAuthentication/MailRequest.java
@@ -0,0 +1,14 @@
+package com.next.genshinflow.application.user.dto.mailAuthentication;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+
+@Getter
+public class MailRequest {
+    @Schema(description = "사용자 이메일", type = "String", example = "user@example.com")
+    @Email
+    @NotEmpty(message = "이메일을 입력해 주세요")
+    private String email;
+}
