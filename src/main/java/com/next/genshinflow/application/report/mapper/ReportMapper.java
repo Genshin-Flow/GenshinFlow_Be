aece8f31e6ef9adc68f2fb964e35aee3fb795592
demuu <preasim.kr@gmail.com>
2024-12-09 16:45:58 +0900

    feat: ReportMapper 추가

---
+++ src/main/java/com/next/genshinflow/application/report/mapper/ReportMapper.java
@@ -0,0 +1,36 @@
+package com.next.genshinflow.application.report.mapper;
+
+import com.next.genshinflow.application.report.dto.CreateReportRequest;
+import com.next.genshinflow.application.report.dto.ReportResponse;
+import com.next.genshinflow.domain.report.entity.ReportEntity;
+import com.next.genshinflow.domain.user.entity.MemberEntity;
+import com.next.genshinflow.enumeration.ReportStatus;
+
+public class ReportMapper {
+    private ReportMapper() {}
+
+    public static ReportEntity toReport(CreateReportRequest reportRequest, MemberEntity reportingUser, MemberEntity targetUser) {
+        ReportEntity report = new ReportEntity();
+        report.setReportingUser(reportingUser);
+        report.setTargetUser(targetUser);
+        report.setContent(reportRequest.getContent());
+        report.setImage(reportRequest.getImage());
+        report.setReportStatus(ReportStatus.UNPROCESSED);
+
+        return report;
+    }
+
+    public static ReportResponse reportToResponse(ReportEntity report) {
+        if (report == null) return null;
+
+        return ReportResponse.builder()
+            .reportingUserId(report.getReportingUser().getId())
+            .targetUserId(report.getTargetUser().getId())
+            .content(report.getContent())
+            .image(report.getImage())
+            .reportStatus(report.getReportStatus())
+            .createdAt(report.getCreatedAt())
+            .completedAt(report.getCompletedAt() != null ? report.getCompletedAt() : null)
+            .build();
+    }
+}
