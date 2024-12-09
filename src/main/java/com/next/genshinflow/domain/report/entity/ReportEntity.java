ab996a16cc5bf1dc0b9e9e46583cdf09407604ff
demuu <preasim.kr@gmail.com>
2024-12-09 16:52:45 +0900

    feat: ReportEntity 추가

--- src/main/java/com/next/genshinflow/domain/report/entity/ReportEntity.java
+++ src/main/java/com/next/genshinflow/domain/report/entity/ReportEntity.java
@@ -1,11 +1,21 @@
 package com.next.genshinflow.domain.report.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.next.genshinflow.domain.BaseEntity;
 import com.next.genshinflow.domain.user.entity.MemberEntity;
+import com.next.genshinflow.enumeration.ReportStatus;
+import com.next.genshinflow.enumeration.converter.ReportStatusConverter;
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "report")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReportEntity extends BaseEntity {
 
     @Id
@@ -13,18 +23,25 @@ public class ReportEntity extends BaseEntity {
     @Column(name = "id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "reporter_id")
-    private MemberEntity reporter;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporting_user")
+    private MemberEntity reportingUser;
 
-    @ManyToOne
-    @JoinColumn(name = "reportee_id")
-    private MemberEntity reportee;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user")
+    private MemberEntity targetUser;
 
-    @Column
+    @Column(name = "content")
     private String content;
 
-//    @Convert(converter = ReportStatusConverter.class)
-    @Column
-    private ReportEntity status;
+    @Column(name = "image")
+    private String image;
+
+    @Convert(converter = ReportStatusConverter.class)
+    @Column(name = "report_status")
+    private ReportStatus reportStatus;
+
+    @Column(name = "completed_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime completedAt;
 }
