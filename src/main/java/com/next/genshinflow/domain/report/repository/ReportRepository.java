1e532ced6bafcdd277ed613158befd7dc0f0844b
demuu <preasim.kr@gmail.com>
2024-12-09 16:52:22 +0900

    feat: ReportRepository 추가

--- src/main/java/com/next/genshinflow/domain/report/repository/ReportRepository.java
+++ src/main/java/com/next/genshinflow/domain/report/repository/ReportRepository.java
@@ -1,12 +1,18 @@
 package com.next.genshinflow.domain.report.repository;
 
 import com.next.genshinflow.domain.report.entity.ReportEntity;
+import com.next.genshinflow.enumeration.ReportStatus;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+
 
 @Hidden
 @Repository
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
-
+    Page<ReportEntity> findByReportStatus(ReportStatus reportStatus, Pageable pageable);
+    Page<ReportEntity> findByTargetUser_Id(long targetId, Pageable pageable);
 }
