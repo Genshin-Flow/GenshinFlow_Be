e8ebe63a892f631a3087d0972555fe4a91734794
demuu <preasim.kr@gmail.com>
2024-12-13 07:03:00 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/build
+REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -23,9 +23,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 0
 echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
