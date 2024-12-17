8cc910171d642b54a1b6f57eb6415d3561fcae41
demuu <preasim.kr@gmail.com>
2024-12-17 18:34:10 +0900

    feat: QuestCategory 추가

--- src/main/java/com/next/genshinflow/enumeration/QuestCategory.java
+++ src/main/java/com/next/genshinflow/enumeration/QuestCategory.java
@@ -1,4 +1,17 @@
 package com.next.genshinflow.enumeration;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum QuestCategory {
+    NORMAL_DOMAIN("일반 비경"),
+    EVENT_QUEST("이벤트 퀘스트"),
+    AREA_CONQUEST("영역 토벌"),
+    DAILY_QUEST("일일 임무"),
+    MAP_EXPLORATION("맵 탐사"),
+    GATHERING("채집");
+
+    private final String category;
 }
