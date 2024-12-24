b83c7e374ea2900a03c96186f07e9eb3a7611b04
demuu <preasim.kr@gmail.com>
2024-12-24 20:42:33 +0900

    로그인 5회 실패 시 계정 잠김 기능 추가

--- src/main/java/com/next/genshinflow/application/user/service/AuthService.java
+++ src/main/java/com/next/genshinflow/application/user/service/AuthService.java
@@ -47,96 +47,97 @@ public class AuthService {
     // 일반 유저 회원가입
     public MemberResponse createMember(SignUpRequest signUpRequest) {
         mailSendService.verifyAuthCode(signUpRequest.getEmail(), signUpRequest.getAuthNum());
-        verifyExistEmail(signUpRequest.getEmail());
+        return saveNewMember(signUpRequest.getUid(), signUpRequest.getEmail(), signUpRequest.getPassword(), false);
+    }
 
-        UserInfoResponse apiResponse = getUserInfoFromApi(signUpRequest.getUid());
+    // OAuth 회원가입
+    public MemberResponse createOAuthMember(OAuthSignUpRequest signUpRequest) {
+        return saveNewMember(signUpRequest.getUid(), signUpRequest.getEmail(), null, true);
+    }
 
-        MemberEntity member = buildMemberEntity(
-            signUpRequest.getUid(),
-            signUpRequest.getEmail(),
-            signUpRequest.getPassword(),
-            apiResponse,
-            false);
+    private MemberResponse saveNewMember(long uid, String email, String password, boolean oauth) {
+        verifyExistEmail(email);
 
+        UserInfoResponse apiResponse = enkaService.getUserInfoFromApi(uid);
+        MemberEntity member = buildMemberEntity(uid, email, password, apiResponse, oauth);
         MemberEntity savedMember = memberRepository.save(member);
 
+        log.info("User {} successfully signed up.", email);
         return MemberMapper.memberToResponse(savedMember);
     }
 
     // 일반 유저 로그인
     public TokenResponse authenticate(LoginRequest loginRequest) {
+        // 로그인 실패 횟수 증가 / 횟수 초과 검증
+        MemberEntity member = handleFailedLoginAttempts(loginRequest);
+
         UsernamePasswordAuthenticationToken authenticationToken =
             new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
 
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        MemberEntity findMember = findMember(loginRequest.getEmail());
-
-        // 게임 상의 프로필 정보 변경 시 업데이트
-        UserInfoResponse apiResponse = getUserInfoFromApi(findMember.getUid());
-        updateMemberIfPlayerInfoChanged(findMember, apiResponse);
-
-        // 제재 유저일 시 제재 기간 확인 및 복구
-        checkAndUpdateDisciplinaryStatus(findMember.getId());
+        // 공통 후처리 작업
+        processPostLoginTasks(member, authentication);
 
         String accessToken = tokenProvider.generateAccessToken(authentication);
-        String refreshToken = tokenProvider.generateRefreshToken(authentication);
-
-        redisRepository.setData(loginRequest.getEmail(), refreshToken, Duration.ofDays(7));
-
-        return new TokenResponse(accessToken, refreshToken);
-    }
-
-    // OAuth 회원가입
-    public MemberResponse createOAuthMember(OAuthSignUpRequest signUpRequest) {
-        verifyExistEmail(signUpRequest.getEmail());
-
-        UserInfoResponse apiResponse = getUserInfoFromApi(signUpRequest.getUid());
-
-        MemberEntity member = buildMemberEntity(
-            signUpRequest.getUid(),
-            signUpRequest.getEmail(),
-            null,
-            apiResponse,
-            true);
-
-        MemberEntity savedMember = memberRepository.save(member);
-
-        return MemberMapper.memberToResponse(savedMember);
+        return new TokenResponse(accessToken, redisRepository.getData(member.getEmail()));
     }
 
     // OAuth 로그인
     public TokenResponse authenticateWithOAuth(OAuthSignInRequest oAuthLoginRequest, String provider) {
-        MemberEntity findMember = findMember(oAuthLoginRequest.getEmail());
+        MemberEntity member = findMember(oAuthLoginRequest.getEmail());
 
-        if (!findMember.getOAuthUser()) {
+        if (!member.getOAuthUser()) {
             throw new BusinessLogicException(ExceptionCode.USER_CANNOT_LOGIN_WITH_OAUTH);
         }
 
         List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
-        Map<String, Object> attributes = Map.of("email", findMember.getEmail());
+        Map<String, Object> attributes = Map.of("email", member.getEmail());
         OAuth2User oAuth2User = new DefaultOAuth2User(authorities, attributes, "email");
 
-        OAuth2AuthenticationToken authenticationToken =
+        OAuth2AuthenticationToken authentication =
             new OAuth2AuthenticationToken(oAuth2User, authorities, provider);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        // 공통 후처리 작업
+        processPostLoginTasks(member, authentication);
 
+        String accessToken = tokenProvider.generateAccessToken(authentication);
+        return new TokenResponse(accessToken, redisRepository.getData(member.getEmail()));
+    }
+
+    private void processPostLoginTasks(MemberEntity member, Authentication authentication) {
         // 게임 상의 프로필 정보 변경 시 업데이트
-        UserInfoResponse apiResponse = getUserInfoFromApi(findMember.getUid());
-        updateMemberIfPlayerInfoChanged(findMember, apiResponse);
+        enkaService.updateMemberIfPlayerInfoChanged(member);
 
         // 제재 유저일 시 제재 기간 확인 및 복구
-        checkAndUpdateDisciplinaryStatus(findMember.getId());
+        checkAndUpdateDisciplinaryStatus(member.getId());
 
-        String accessToken = tokenProvider.generateAccessToken(authenticationToken);
-        String refreshToken = tokenProvider.generateRefreshToken(authenticationToken);
+        // 로그인 성공 시 Redis에 RefreshToken 저장
+        String refreshToken = tokenProvider.generateRefreshToken(authentication);
+        redisRepository.setData(member.getEmail(), refreshToken, Duration.ofDays(7));
 
-        redisRepository.setData(oAuthLoginRequest.getEmail(), refreshToken, Duration.ofDays(7));
+        // 로그인 실패 횟수 초기화
+        member.setFailedLoginAttempts(0);
+        memberRepository.save(member);
+    }
 
-        return new TokenResponse(accessToken, refreshToken);
+    private MemberEntity handleFailedLoginAttempts(LoginRequest loginRequest) {
+        MemberEntity member = memberRepository.findByEmail(loginRequest.getEmail())
+            .orElseThrow(() -> new BusinessLogicException(ExceptionCode.INVALID_CREDENTIALS));
+
+        if (member.getFailedLoginAttempts() >= 5) {
+            throw new BusinessLogicException(ExceptionCode.LOGIN_ATTEMPTS_EXCEEDED);
+        }
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), member.getPassword())) {
+            member.setFailedLoginAttempts(member.getFailedLoginAttempts() + 1);
+            memberRepository.save(member);
+            throw new BusinessLogicException(ExceptionCode.INVALID_CREDENTIALS);
+        }
+
+        return member;
     }
 
     // 리프레시 토큰을 통한 액세스 토큰 재발급
@@ -168,47 +169,8 @@ public class AuthService {
         }
 
         member.setPassword(passwordEncoder.encode(password));
+        member.setFailedLoginAttempts(0);
         memberRepository.save(member);
-    }
-
-    // 원신 api 유저 정보 요청
-    public UserInfoResponse getUserInfoFromApi(long uid) {
-        UserInfoResponse apiResponse = enkaService.fetchUserInfoFromApi(uid);
-
-        if (apiResponse == null || apiResponse.getPlayerInfo() == null) {
-            throw new BusinessLogicException(ExceptionCode.EXTERNAL_API_ERROR);
-        }
-        return apiResponse;
-    }
-
-    // 받아온 api를 조회해 변경된 유저 정보가 있으면 업데이트
-    public void updateMemberIfPlayerInfoChanged(MemberEntity member, UserInfoResponse apiResponse) {
-        UserInfoResponse.PlayerInfo playerInfo = apiResponse.getPlayerInfo();
-        if (playerInfo == null) return;
-        boolean isUpdated = false;
-
-        if (!member.getName().equals(playerInfo.getNickname())) {
-            member.setName(playerInfo.getNickname());
-            isUpdated = true;
-        }
-        if (member.getLevel() != playerInfo.getLevel()) {
-            member.setLevel(playerInfo.getLevel());
-            isUpdated = true;
-        }
-        if (member.getWorldLevel() != playerInfo.getWorldLevel()) {
-            member.setWorldLevel(playerInfo.getWorldLevel());
-            isUpdated = true;
-        }
-
-        String tower = playerInfo.getTowerFloorIndex() + "-" + playerInfo.getTowerLevelIndex();
-        if (!member.getTowerLevel().equals(tower)) {
-            member.setTowerLevel(tower);
-            isUpdated = true;
-        }
-
-        if (isUpdated) {
-            memberRepository.save(member);
-        }
     }
 
     private MemberEntity buildMemberEntity(long uid, String email, String password, UserInfoResponse apiResponse, boolean oauth) {
